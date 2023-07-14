232dc30f199b790b6bc5f407e3a487d96fb824fa
Bogdan <Meln>
2023-07-14 11:21:59 +0600

    added user context and con strings

---
+++ DictionaryApplication/Contexts/UserDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DictionaryApplication.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DictionaryApplication.Contexts;
+
+public class UserDbContext : IdentityDbContext
+{
+    public UserDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+}
--- DictionaryApplication/Program.cs
+++ DictionaryApplication/Program.cs
@@ -1,6 +1,13 @@
+using DictionaryApplication.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<UserDbContext>( options =>
+{
+    options.UseSqlServer(builder.Configuration.GetSection("ConnectionStrings:UsersConnection").Value);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
